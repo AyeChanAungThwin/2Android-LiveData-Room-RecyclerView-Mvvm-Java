35d96a99f4026dc1c5030121a3c9abefe80bc75f
Aye Chan Aung Thwin <ayechanaungthwin@gmail.com>
2020-01-27 22:19:49 +0630

    updated

--- app/src/main/java/com/acat/java/livedata/room/recyclerview/MainActivity.java
+++ app/src/main/java/com/acat/java/livedata/room/recyclerview/MainActivity.java
@@ -3,10 +3,13 @@ package com.acat.java.livedata.room.recyclerview;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.acat.java.livedata.room.recyclerview.adapter.NoteAdapter;
 import com.acat.java.livedata.room.recyclerview.entity.Note;
 import com.acat.java.livedata.room.recyclerview.viewmodel.NoteViewModel;
 
@@ -21,12 +24,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this  ));
+        recyclerView.setHasFixedSize(true);
+
+        final NoteAdapter adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
+
         noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
                 //update RecyclerView
-                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+                adapter.setNotes(notes);
             }
         });
     }
